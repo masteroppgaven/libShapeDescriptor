188e61a24dee6fc62bc7fef60465805a75d03706
Jonathan Brooks <brooks.jonathan1@gmail.com>
2023-04-01 15:36:08 +0800

    Remove unused files

--- tools/benchmarking/utilities/distance/pearson.cpp
+++
@@ -1,6 +0,0 @@
-#include <shapeDescriptor/cpu/spinImageGenerator.h>
-#include <shapeDescriptor/cpu/types/array.h>
-
-double pearsonCorrelationSimilarity(ShapeDescriptor::SpinImageDescriptor dOne, ShapeDescriptor::SpinImageDescriptor dTwo)
-{
-}
